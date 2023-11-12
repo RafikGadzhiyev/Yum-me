45492140c41e13286c076b7a3ce18abf0630cf51
Rafik <rafik.gadzhiyev.03@bk.ru>
2023-11-12 19:43:42 +0600

    fix: add plug for future api's

--- app/api/storage/text_generation/route.ts
+++ app/api/storage/text_generation/route.ts
@@ -1,7 +1,9 @@
+import { handleRequest } from "@/utils/handlers.util";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest, res: NextResponse) => {
 	try {
+		return handleRequest(null, null, 200);
 	} catch (e) {
 		return NextResponse.json(e, { status: 403 });
 	}
