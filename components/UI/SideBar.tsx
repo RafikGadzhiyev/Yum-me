e77a1ea17544ccd594d5161a72a85bfee8caf1f5
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-01-03 00:08:13 +0600

    style: fix trouble with mobile version of  layout

--- components/UI/SideBar.tsx
+++ components/UI/SideBar.tsx
@@ -2,7 +2,7 @@ import { SideBarNavigation } from "../feature/SideBarNavigation";
 
 export const SideBar = () => {
 	return (
-		<aside className="flex p-3 px-5 rounded-r-md bottom-0 bg-green-300 md:static md:w-auto">
+		<aside className="fixed bottom-0 top-0 z-50 flex h-min w-full rounded-r-md bg-green-300 p-3 px-5 md:static md:h-[100vh] md:w-auto">
 			<SideBarNavigation />
 		</aside>
 	);
--- components/layouts/ScrollableLayout.tsx
+++ components/layouts/ScrollableLayout.tsx
@@ -2,6 +2,8 @@ import { FC, PropsWithChildren } from "react";
 
 export const ScrollableLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div className="max-h-[100vh] h-[100vh] w-full overflow-y-auto">{children}</div>
+		<div className="max-h-[100vh] w-full overflow-y-auto pt-20 md:pt-0">
+			{children}
+		</div>
 	);
 };
