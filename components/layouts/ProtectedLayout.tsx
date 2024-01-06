272739513f060aefd3c16ebf15573e46a4f7ed48
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-01-06 17:32:42 +0600

    feat: add user heath data read logic

--- components/layouts/ProtectedLayout.tsx
+++ components/layouts/ProtectedLayout.tsx
@@ -4,8 +4,14 @@ import { FC, PropsWithChildren, useCallback, useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { AUTH_ROUTES, ROUTES } from "@/consts/routes.const";
 import { getSession } from "@/api/auth";
+import { useDispatch } from "react-redux";
+import { readUser } from "@/redux/slices/user.slice";
+import { Query, UserActiveSession } from "@/lib/appwrite";
+import { getUsers } from "@/api/user";
+import { readUserHealthData } from "@/redux/slices/userHealthData.slice";
 
 export const ProtectedLayout: FC<PropsWithChildren> = ({ children }) => {
+	const dispatch = useDispatch();
 	const router = useRouter();
 	const pathname = usePathname();
 
@@ -17,7 +23,11 @@ export const ProtectedLayout: FC<PropsWithChildren> = ({ children }) => {
 		try {
 			const user = await getSession();
 
+			dispatch(readUser(user));
+
 			if (user && authRoutesPathList.includes(pathname)) {
+				await getUserHealth(user);
+
 				router.push(ROUTES.HOME.path);
 			}
 		} catch (err) {
@@ -26,6 +36,15 @@ export const ProtectedLayout: FC<PropsWithChildren> = ({ children }) => {
 			}
 		}
 	}, [pathname, router]);
+
+	const getUserHealth = async (user: UserActiveSession) => {
+		const users = await getUsers([Query.equal("email", user.email)]);
+		const userHealthData = users[0];
+
+		dispatch(readUserHealthData(userHealthData));
+
+		return userHealthData;
+	};
 
 	useEffect(() => {
 		getUser();
