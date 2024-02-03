f5712fa1427ec612b4493e6cc70085a7e8f24045
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-02-04 02:07:57 +0600

    refactor: return prisma connection creation logic

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,31 @@
+// import { PrismaClient } from "@prisma/client";
+//
+// declare global {
+// 	// eslint-disable-next-line no-var
+// 	var prisma: PrismaClient | undefined;
+// }
+//
+// const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+//
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
 
 declare global {
 	// eslint-disable-next-line no-var
-	var prisma: PrismaClient | undefined;
+	var prisma: PrismaClient;
 }
 
-const prisma = global.prisma || new PrismaClient({ log: ["info"] });
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+	prisma = new PrismaClient();
+} else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient();
+	}
+	prisma = global.prisma;
+}
 
 export default prisma;
