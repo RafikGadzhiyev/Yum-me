06b0412d34a5fbeae6f91d5f117acc1eb1f5e91c
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-02-04 14:39:28 +0600

    hotfix: issue with prisma on production

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import prisma from "@/lib/prisma";
+
 export const GET = async (req: NextRequest) => {
 	try {
 		const searchParams = req.nextUrl.searchParams;
