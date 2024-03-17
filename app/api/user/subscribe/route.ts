be718a48625e87858875d044d18d9cf45602a1c3
Rafik <rafik.gadzhiyev.03@bk.ru>
2024-03-17 20:52:11 +0500

    lint: fix issues

--- app/api/user/subscribe/route.ts
+++ app/api/user/subscribe/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { handleRequest } from "@/utils/handlers.util";
 
 export const PATCH = async (req: NextRequest) => {
